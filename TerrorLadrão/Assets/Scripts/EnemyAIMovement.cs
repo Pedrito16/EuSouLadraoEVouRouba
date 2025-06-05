d9e3dfdad74aebb3387d0fb7e5c5102eded6c9a5
Pedrito16 <proibidofantasma@gmail.com>
2025-06-05 14:10:04 -0300

    fiz a ia do inimigo ser mais aceitavel, falta arrumar mais o cenário
    fazer o interior da cupula, fazer a cutscene inicial, organizar hierarquia, fazer o diamante, etc

--- TerrorLadrão/Assets/Scripts/EnemyAIMovement.cs
+++ TerrorLadrão/Assets/Scripts/EnemyAIMovement.cs
@@ -1,60 +1,143 @@
 using StarterAssets;
 using UnityEngine;
 using UnityEngine.AI;
-
+using System.Collections;
 public enum EnemyState
 {
-    Idle,
+    LookingAtPlayer,
     Patrol,
-    Searching,
-    Attack
+    Wait,
 }
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyAIMovement : MonoBehaviour
 {
-    [SerializeField] float anguloVisão = 60;
-    [SerializeField] float distanciaVisão = 10;
-    [SerializeField] EnemyState state;
-    [SerializeField] Transform[] waypoints;
+    [Header("Optional")]
     [SerializeField] Transform head;
+    [Tooltip("OPCIONAL: transform da cabeça do inimigo que faz a cabeça dele olhar para o player")]
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] new Light light;
+
+    [Header("Configurable")]
+    [SerializeField] float anguloVisão = 60; //angulo de raio para ter apenas o cone de visão
+    [SerializeField] float distanciaVisão = 10; //distancia em que o cone pode pegar
+    [SerializeField] float waitingTime = 0.85f;
+    
+    [Header("Read-Only")]
+    [SerializeField] int walkingToWaypoint;
+    [SerializeField] EnemyState state;
+
+    //variaveis privadas
     NavMeshAgent agent;
     Transform target;
     Vector3 direction;
     float angle;
     float distance;
+    bool isLookingAtPlayer;
+    Color originalLightColor;
+    float originalIntensity = 50f;
+    Animator animator;
     void Start()
     {
+        animator = GetComponent<Animator>();
+        originalLightColor = light.color;
         target = FirstPersonController.instance.transform;
+        light.intensity = originalIntensity;
         agent = GetComponent<NavMeshAgent>();
-        
+        SwitchState(EnemyState.Patrol);
+        if(waypoints[walkingToWaypoint] != null)
+        agent.SetDestination(waypoints[walkingToWaypoint].position);
     }
     void Update()
     {
         direction = target.position - transform.position;
-            
         distance = Vector3.Distance(transform.position, target.position);
-        print(direction);
+
         if (distance < distanciaVisão)
         {
             angle = Vector3.Angle(direction, transform.forward);
-            if (angle < anguloVisão)
+            if (angle < anguloVisão && !isLookingAtPlayer)
             {
-                agent.SetDestination(target.position);
+                isLookingAtPlayer = true;
+                SwitchState(EnemyState.LookingAtPlayer);
+            } else if( angle > anguloVisão && isLookingAtPlayer)
+            {
+                isLookingAtPlayer = false;
+                StartCoroutine(WaitState(EnemyState.Patrol));
             }
+
+        }else if(distance >= distanciaVisão && isLookingAtPlayer)
+        {
+            isLookingAtPlayer = false;
+            StartCoroutine(WaitState(EnemyState.Patrol));
         }
         switch (state)
         {
-            case EnemyState.Idle:
-                // Lógica para estado Idle
-                break;
+            case EnemyState.Patrol:
+                if (agent.remainingDistance <= 0.5f)
+                {
+                    walkingToWaypoint++;
+                    walkingToWaypoint = CheckIfShouldResetPatrol(walkingToWaypoint);
+                    agent.SetDestination(waypoints[walkingToWaypoint].position);
+                }
+                    break;
         }
     }
     private void LateUpdate()
     {
-        if(distance < distanciaVisão)
+        if (head == null) return;
+
+        if (distance < distanciaVisão)
         {
-            head.rotation = Quaternion.LookRotation(direction);
+            if (angle < anguloVisão)
+            {
+                head.rotation = Quaternion.LookRotation(direction);
+            }
         }
     }
+    void SwitchState(EnemyState newState)
+    {
+        switch (newState)
+        {
+            case EnemyState.LookingAtPlayer:
+                agent.enabled = false;
+                light.color = new Color(248, 98, 15);
+                light.intensity = 0.001f;
+                animator.SetBool("IsPatrolling", false);
+                break;
+            case EnemyState.Patrol:
+                animator.SetBool("IsPatrolling", true);
+                break;
+            case EnemyState.Wait:
+                StartCoroutine(WaitState(EnemyState.Patrol));
+                break;
+        }
+        state = newState;
+    }
+    IEnumerator WaitState(EnemyState stateToGoTo)
+    {        
+        yield return new WaitForSeconds(waitingTime);
+        head.rotation = Quaternion.identity;
+        agent.enabled = true;
+        light.color = originalLightColor;
+        light.intensity = originalIntensity;
+        if (stateToGoTo == EnemyState.Patrol) animator.SetBool("IsPatrolling", true);
+        state = stateToGoTo;
+    }
+    int CheckIfShouldResetPatrol(int value)
+    {
+        value = Mathf.Clamp(value, 0, waypoints.Length);
+        if (value < waypoints.Length)
+        {
+            return value;
+        }
+        else if (value >= waypoints.Length)
+        {
+            value = 0;
+            return value;
+        }
+        else return 0;
+    }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
