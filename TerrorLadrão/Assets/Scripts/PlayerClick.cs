b06a8b2934fe018371b1f61c78b9b3e4f7472034
Pedrito16 <pedrogabrielvicente98@gmail.com>
2025-06-12 08:49:58 -0300

    o gpt me ajudou a resolver o raycast, e falta pegar o diamante

--- TerrorLadrão/Assets/Scripts/PlayerClick.cs
+++ TerrorLadrão/Assets/Scripts/PlayerClick.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayerClick : MonoBehaviour
 {
     [SerializeField] Transform cameraRoot;
+    public GraphicRaycaster raycaster;
+    public EventSystem eventSystem;
     void Start()
     {
         
@@ -10,16 +15,21 @@ public class PlayerClick : MonoBehaviour
 
     void Update()
     {
-        Ray raio = new Ray(cameraRoot.transform.position, cameraRoot.transform.forward);
-        RaycastHit hit;
-        Debug.DrawRay(cameraRoot.transform.position, cameraRoot.transform.forward);
-        if (Physics.Raycast(raio, out hit))
+        if (Input.GetMouseButtonDown(0))
         {
-            var button = hit.collider.GetComponent<UnityEngine.UI.Button>();
-            if (button != null && Input.GetMouseButtonDown(0))
+            PointerEventData pointerEventData = new PointerEventData(eventSystem);
+            pointerEventData.position = new Vector2(Screen.width / 2, Screen.height / 2);
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            raycaster.Raycast(pointerEventData, results);
+
+            foreach (RaycastResult result in results)
             {
-                print("Clicando");
-                button.onClick.Invoke();
+                Button button = result.gameObject.GetComponent<Button>();
+                if (button != null)
+                {
+                    button.onClick.Invoke();
+                }
             }
         }
     }
